c1019f01e4b6931617d4df03f400a908bc93f5e9
jiteshtask <Affle@123>
2019-12-26 17:59:52 +0530

    creating routing component

--- src/index.jsx
+++ src/index.jsx
@@ -1,14 +1,7 @@
 import React from "react";
 import {render} from "react-dom";
 
-import App from "./App";
-import LifeCycle from './LifeCycle';
-import Component from  './Component';
-import AddingStateToComponent from './AddingStateToComponent';
-import EventHandling from './EventHandling';
-import ReactBuildingBlocks from './ReactBuildingBlocks';
 import MyRoutes from './AppRoutes'
-import DefaultProps from './DefaultProps';
 import * as style from "./index.css";
 
 
