794bf842c18fc90c9a096c0f801b2ef819ed5ba3
jiteshtask <Affle@123>
2019-12-26 18:01:59 +0530

    react lifeCycle method

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,12 @@
 import React from "react";
 import {render} from "react-dom";
 
-import App from "./App";
 import LifeCycle from './LifeCycle';
-import Component from  './Component';
-import AddingStateToComponent from './AddingStateToComponent';
-import EventHandling from './EventHandling';
-import ReactBuildingBlocks from './ReactBuildingBlocks';
-import MyRoutes from './AppRoutes'
-import DefaultProps from './DefaultProps';
 import * as style from "./index.css";
 
 
 window.onload = () => {
   document.body.innerHTML = '<div id="root"/>';
-  render(<MyRoutes />, document.getElementById("root"));
+  render(<LifeCycle />, document.getElementById("root"));
   module.hot.accept();
 };
