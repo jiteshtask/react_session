10339c9a3a35a64aed70ce624856a8518dcfdef0
jiteshtask <Affle@123>
2019-12-27 18:51:09 +0530

    components

--- src/Component.jsx
+++ src/Component.jsx
@@ -1,17 +1,40 @@
-import React from "react";
+import React, { Component } from "react";
 
-class App extends React.Component {
+class App extends Component {
     render() {
       return (
         <h1>Welcome to React session</h1>
       )
     }
   }
+
+
+/* STETEFULL COMPONENT*/ 
+// class App extends Component {
+//   constructor(props){
+//     super(props);
+//     this.state ={
+//       number: 2
+//     }
+//   }
+
+//   render(){
+//     console.log("rendering")
+//     return(
+//     <h1>{this.state.number}</h1>
+//     )
+//   }
+// }  
+
   
+/* STETELESS COMPONENT*/   
 //   const App = () =>{
 //    return (
 //       <h1>Welcome to React session</h1>
 //    )
-//   };  
+//   };
+
+
+
 
 export default App;
